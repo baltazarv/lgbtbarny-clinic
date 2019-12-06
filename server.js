ff9c71347c0d42f81542cce2be5d971ccfab7112
Baltazar Villegas <baltazarv@gmail.com>
2019-12-06 10:49:58 -0500

    More changes for simple HTTP auth.

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+
 const app = express();
 
 dotenv.config({
 	path: './config.env'
 });
+
+const auth = require('http-auth');
+
+const basic = auth.basic({
+	file: `${__dirname}/.htpasswd`
+});
+app.use(auth.connect(basic));
 
 // process.env.PORT: port injected by web host
 const port = process.env.PORT || 8080;
@@ -15,9 +23,9 @@ const port = process.env.PORT || 8080;
 const Email = require('./utils/Email');
 /* to read file from server */
 // const getTemplate = require('./utils/getTemplate');
-const env = nunjucks.configure(['views', 'views/email'], {
+nunjucks.configure(['views', 'views/email'], {
 	autoescape: true,
-	express: app,
+	express: app
 });
 
 app.use(bodyParser.json());
@@ -27,18 +35,19 @@ app.post('/api/v1/sendemail', async (req, res) => {
 		/** if need to read file from server, require './utils/getTemplate' **/
 		// const template = await getTemplate(req.body.filename);
 
-
-		const customTxtHtmlBR = req.body.customText.replace(/(\r\n|\n|\r)/g, '<br />')
+		const customTxtHtmlBR = req.body.customText.replace(
+			/(\r\n|\n|\r)/g,
+			'<br />'
+		);
 		const template = nunjucks.render('email-visitor.html', {
 			custommsg: customTxtHtmlBR
 		});
-		const email = new Email(template, req.body).send();
+		new Email(template, req.body).send();
 
 		res.status(200).json({
 			status: 'success',
 			message: 'Email sent to Mailtrap'
-		})
-
+		});
 	} catch (err) {
 		console.log('post err', err);
 	}
@@ -51,14 +60,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
-
-	// basic HTTP authentication
-	const auth = require('http-auth');
-	const basic = auth.basic({
-		realm: 'LeGaL Clinic',
-		file: __dirname + '/.htpasswd'
-	});
-	app.use(auth.connect(basic));
 }
 
-app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
+app.listen(port, () =>
+	console.log(`App listening on http://localhost:${port}`)
+);
